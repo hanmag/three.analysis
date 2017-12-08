24b621ba660d20ed28d421888ae5bcfe0b2aa9e0
wangjue <wangjue1199@163.com>
2017-12-08 13:52:40 +0800

    remove d3-scale and tinycolor2

--- src/utils/color-utils.js
+++ src/utils/color-utils.js
@@ -1,12 +1,8 @@
-import { schemePaired } from 'd3-scale-chromatic';
-import tinyColor from 'tinycolor2';
-
-const colorStr2Hex = str => isNaN(str) ? parseInt(tinyColor(str).toHex(), 16) : str;
-
 function autoColorItems(items, colorField) {
     if (typeof colorField !== 'string') return;
 
-    const colors = schemePaired; // Paired color set from color brewer
+    // d3.scale.category10
+    const colors = [0x1f77b4, 0xff7f0e, 0x2ca02c, 0xd62728, 0x9467bd, 0x8c564b, 0xe377c2, 0x7f7f7f, 0xbcbd22, 0x17becf];
 
     const colorGroups = {};
 
@@ -22,4 +18,6 @@ function autoColorItems(items, colorField) {
     });
 }
 
-export { autoColorItems, colorStr2Hex };
+export {
+    autoColorItems
+};
