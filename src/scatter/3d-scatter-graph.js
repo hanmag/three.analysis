557516a3f241a2a9c87b443e3927c398d91fcf9a
wangjue <wangjue1199@163.com>
2017-12-12 19:21:43 +0800

    draw plane

--- src/scatter/3d-scatter-graph.js
+++ src/scatter/3d-scatter-graph.js
@@ -4,18 +4,40 @@ import nodeFragmentShaderSource from '../shaders/node.fragment.glsl';
 import * as TWEEN from 'es6-tween';
 import * as THREE from 'three';
 
-const CAMERA_DISTANCE2NODES_FACTOR = 140;
+const CAMERA_DISTANCE2NODES_FACTOR = 90;
 const NODE_BASE_SIZE = 4;
-const LINEMATERIAL = new THREE.LineBasicMaterial({
-    color: 0xffffff,
+const PLANEMATERIAL = new THREE.MeshBasicMaterial({
+    color: 0xdddddd,
+    side: THREE.DoubleSide,
     transparent: true,
-    opacity: 0.15
+    opacity: 0.2
 });
+const LEFT_PLANE = new THREE.Mesh(new THREE.PlaneGeometry(200, 120, 1), PLANEMATERIAL);
+
+function drawCords(state) {
+    // Re-drawing 3d-plane
+    for (let i = 0; i < state.webglScene.children.length; i++) {
+        if (state.webglScene.children[i] && state.webglScene.children[i].type === 'Plane')
+            state.webglScene.remove(state.webglScene.children[i--])
+    }
+
+    state.webglScene.add(LEFT_PLANE);
+
+    new TWEEN.Tween(state.camera.position).to({
+        x: 0,
+        y: 0,
+        z: Math.cbrt(state.graphData.length) * CAMERA_DISTANCE2NODES_FACTOR
+    }, 600).easing(TWEEN.Easing.Quadratic.InOut).on(() => {
+        state.camera.lookAt(state.webglScene.position);
+    }).start();
+}
 
 export default {
     name: 'scatter',
     apply: function (state) {
-        // Setup graph
+        // draw coordinate system
+        drawCords(state);
+
         if (state.resetData) {
             this.layout(state);
         } else {
